190d3d2dd27b55b516ce2bca3d8044183e2dfd4a
Altaf khan <altafpathan439@gmail.com>
2023-01-03 12:53:41 +0530

    this is first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+function App() {
+  return (
+    <div className='App' >
+      <h1> this is my first page </h1>
+      <div className='btn'>
+        <button>like</button>
+        <button>subscribe</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
